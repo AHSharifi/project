97a40eb4de12bb82b0f02cc7b54c1083f9f7a69e
AHSharifi <mr.amirhossein88@gmail.com>
2024-03-17 12:39:34 +0330

    edit show.js

--- show.js
+++ show.js
@@ -17,11 +17,40 @@ fetch('./config.json')
     if (item) {
       const container = document.getElementById('box');
       container.innerHTML = `
-        <h2>${item.name}</h2>
-        <p>${item.description}</p>
+        <h1>${item.name}</h1>
+        <p class="description">${item.description}</p>
         <p>Price: ${item.price}$</p>
+        <select id="colorSelect">
+          ${
+            item.color.map(color => {
+              return `<option>${color}</option>`;
+            }).join('')
+          }
+        </select>
+        <select id="sizeSelect">
+          ${
+            item.size.map(size => {
+              return `<option>${size}</option>`
+            }).join('')
+          }
+        </select>
         <img src="${item.image}" alt="${item.name}">
+        <button id="btn">Add to cart</button>
       `;
+
+      const btn = document.getElementById('btn')
+      btn.addEventListener('click', () => {
+        const colorValue = document.getElementById('colorSelect').value
+        const sizeValue = document.getElementById('sizeSelect').value
+
+        const cart = {
+          name: document.getElementsByTagName('h1')[0].textContent,
+          price: document.getElementsByTagName('p')[1].textContent,
+          color: colorValue,
+          size: sizeValue,
+          img: document.getElementsByTagName('img')[0].src
+        }
+      })
     } else {
       alert('item not found!')
       console.error('آیتم مورد نظر یافت نشد');
